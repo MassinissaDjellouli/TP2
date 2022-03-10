0d412413681b9502821c2fa5723a8d445135f3b6
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-03-10 16:29:26 -0500

    implémentation de save et création de méthodes privées pour la fonction save du DAOEmploye

--- src/main/java/Database/DAOEmploye.java
+++ src/main/java/Database/DAOEmploye.java
@@ -2,15 +2,20 @@ package Database;
 
 import Models.Users.Employe;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
 
 public class DAOEmploye implements DAOInterface<Employe> {
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TP2");
+    EntityManager entityManager;
     @Override
     public void save(Employe toSave) {
-
+        throwIfNull(toSave);
+        beginTransaction();
+        entityManager.persist(toSave);
+        finishTransaction();
     }
 
     @Override
@@ -31,5 +36,23 @@ public class DAOEmploye implements DAOInterface<Employe> {
     @Override
     public void delete(Employe toDelete) {
 
+    }
+
+    private EntityManager getEntityManager(){
+        return entityManagerFactory.createEntityManager();
+    }
+
+    private void throwIfNull(Object o){
+        if(o == null) throw new IllegalArgumentException();
+    }
+    private void beginTransaction(){
+        entityManager = getEntityManager();
+        entityManager.getTransaction().begin();
+    }
+    private void finishTransaction(){
+        throwIfNull(entityManager);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManager = null;
     }
 }
