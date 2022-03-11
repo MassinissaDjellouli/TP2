429a52c9ce228a7b0991e09664c69a482ca2ed88
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-03-11 10:36:02 -0500

    Création d'une superclasse abstract DAO qui contient les fonctions communes utilisées par tout les DAO

---
+++ src/main/java/Database/DAO.java
@@ -0,0 +1,27 @@
+package Database;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class DAO {
+    protected EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TP2");
+    protected EntityManager entityManager;
+    protected EntityManager getEntityManager(){
+        return entityManagerFactory.createEntityManager();
+    }
+
+    protected void throwIfNull(Object o){
+        if(o == null) throw new IllegalArgumentException();
+    }
+    protected void beginTransaction(){
+        entityManager = getEntityManager();
+        entityManager.getTransaction().begin();
+    }
+    protected void finishTransaction(){
+        throwIfNull(entityManager);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManager = null;
+    }
+}
